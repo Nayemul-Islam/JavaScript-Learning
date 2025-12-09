df58e6996d472ff04ddc1d7fc7779b4ce033dbdd
Mohammad Nayemul Islam <nayem1010s@gmail.com>
2025-12-09 18:47:18 +0600

    Variables Module added

---
+++ 02-variables.js
@@ -0,0 +1,16 @@
+// In JavaScript there are 4 ways to declaring variables
+// 1. const  (This variables cannot be changed in future)
+// 2. let (value canbe change in future)
+// 3. var (same age let , before ES6 var used for declaring variables)
+// 4. you can decalre variables as like as x = 5; (not recommended)
+
+const name = "Nayem";
+let age = 10;
+var x = 5;
+y = 6;
+
+console.log(`Hi! My name is ${name}. My age is ${age}. I am a student`);
+console.log(`Sum of X and Y is ${x + y}`);
+
+
+
