efdf65ff8602d37a7ab7be7183f5557ee962efb9
Mohammad Nayemul Islam <nayem1010s@gmail.com>
2025-12-10 17:59:59 +0600

    some changed in array.js file

--- 07-array.js
+++ 07-array.js
@@ -1,12 +1,12 @@
 const fruits = ["apple", "banana", "oragne", "plam", "water mellon"];
 
-for(let fruit of fruits){
-    console.log(fruit);
+for (let fruit of fruits) {
+  console.log(fruit);
 }
 fruits[0] = "Apple";
 fruits.push("Mango", "Blackberry");
 console.log(fruits);
 
-
 console.log(fruits);
 
+console.log(fruits.slice(0, 4));
